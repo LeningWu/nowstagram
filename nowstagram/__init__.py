ed4388f87ff1e173648a9325b50d4f5bb60413c0
wulening <wulening@foxmail.com>
2018-10-19 15:57:18 +0800

    lesson2

--- nowstagram/__init__.py
+++ nowstagram/__init__.py
@@ -11,7 +11,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.jinja_env.add_extension('jinja2.ext.loopcontrols')   # 可以在html文件中使用jinja的break语句，
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost:3306/test2?charset=utf8mb4'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
